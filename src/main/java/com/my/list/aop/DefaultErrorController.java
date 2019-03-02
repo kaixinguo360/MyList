a7fa230727d5e5295a3f55db5b982e12d6e2bd98
Kaixinguo <kaixinguo@kaixinguo.site>
2019-03-02 14:34:19 +0800

    Add Angular WebApp

--- src/main/java/com/my/list/aop/DefaultErrorController.java
+++ src/main/java/com/my/list/aop/DefaultErrorController.java
@@ -3,10 +3,10 @@ package com.my.list.aop;
 import com.my.list.controller.RequestException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -15,13 +15,24 @@ public class DefaultErrorController implements ErrorController {
     private static final String ERROR_PATH = "/error";
 
     @RequestMapping(ERROR_PATH)
-    public ResponseEntity handleError(HttpServletRequest request) throws RequestException {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        HttpStatus status = HttpStatus.resolve(statusCode);
-        if (status == null) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
+    public Object handleError(HttpServletRequest request) throws RequestException {
+
+        Object statusCode = request.getAttribute("javax.servlet.error.status_code");
+        Object url = request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (statusCode instanceof Integer) {
+            if (url instanceof String && ((String) url).startsWith("/api/")) {
+                HttpStatus status = HttpStatus.resolve((int) statusCode);
+                if (status == null) {
+                    status = HttpStatus.INTERNAL_SERVER_ERROR;
+                }
+                throw new RequestException(status.getReasonPhrase(), status);
+            } else {
+                return "/";
+            }
+        } else {
+            return "/";
         }
-        throw new RequestException(status.getReasonPhrase(), status);
     }
 
     @Override
