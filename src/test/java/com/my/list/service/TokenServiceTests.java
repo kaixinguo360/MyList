75c07e8ff65fcf083ec97049edfb290b3e674e18
Kaixinguo <kaixinguo@kaixinguo.site>
2019-03-02 18:44:26 +0800

    Update Config File

--- src/test/java/com/my/list/service/TokenServiceTests.java
+++ src/test/java/com/my/list/service/TokenServiceTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertFalse;
@@ -13,6 +14,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TokenServiceTests {
 
     @Autowired
