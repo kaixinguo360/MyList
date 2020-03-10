9ce68d66423695fd9f58f804b6c5bd126f864e61
Kaixinguo <kaixinguo@kaixinguo.site>
2020-03-10 23:59:51 +0800

    Change proxy hook api

--- src/main/java/com/my/list/controller/ProxyController.java
+++ src/main/java/com/my/list/controller/ProxyController.java
@@ -8,7 +8,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Base64;
 import java.util.Enumeration;
@@ -29,12 +29,8 @@ import java.util.Enumeration;
 public class ProxyController {
 
     private static final String HOOK; static {
-        HOOK = "<script>ORIGIN='%s'</script>\n" +
-            "<script>PATH='%s'</script>\n" +
-            "<script src='//code.jquery.com/jquery-3.3.1.min.js'></script>\n" +
-            "<script src='//unpkg.com/xhook@latest/dist/xhook.min.js'></script>\n" +
-            "<link rel='stylesheet' href='/proxy/hook/hook.css'></link>\n" +
-            "<script src='/proxy/hook/hook.js'></script>\n";
+        HOOK = "<script>ORIGIN='%s';PATH='%s';</script>\n" +
+            "<script src='/proxy/hook.js'></script>\n";
     }
     private final Base64.Encoder encoder = Base64.getUrlEncoder();
     private final Base64.Decoder decoder = Base64.getUrlDecoder();
@@ -51,20 +47,24 @@ public class ProxyController {
         String urlStr = decodeBase64Url(url64);
         
         // Get url
-        URL url = null;
-        try { url = new URL(urlStr); } catch (MalformedURLException ignored) {}
+        URL url;
+        try {
+            url = new URL(urlStr);
+        } catch (MalformedURLException ignored) {
+            throw new DataException("Bad Request, parse url failed, urlStr='" + urlStr + "'");
+        }
 
         // Get method
         HttpMethod method = HttpMethod.resolve(request.getMethod());
         method = method == null ? HttpMethod.GET : method;
         
         // Get headers
-        HttpHeaders headers = getHeaders(request, url == null ? request.getRemoteHost() : url.getHost());
+        HttpHeaders headers = getHeaders(request, url);
 
         // Exchange request
         try {
             HttpEntity<byte[]> exchangeRequest = new HttpEntity<>(body, headers);
-            return restTemplate.exchange(urlStr, method, exchangeRequest, byte[].class);
+            return restTemplate.exchange(URI.create(urlStr), method, exchangeRequest, byte[].class);
         } catch (RestClientResponseException e) {
             HttpStatus status = HttpStatus.resolve(e.getRawStatusCode());
             status = status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status;
@@ -87,7 +87,7 @@ public class ProxyController {
         try {
             url = new URL(urlStr);
             Connection connect = Jsoup.connect(urlStr);
-            HttpHeaders headers = getHeaders(request, url.getHost());
+            HttpHeaders headers = getHeaders(request, url);
             headers.set("Accept-Encoding", "gzip, deflate");
             Connection data = connect.headers(headers.toSingleValueMap());
             doc = data.get();
@@ -104,38 +104,21 @@ public class ProxyController {
         } else {
             doc.head().append("<meta name=\"referrer\" content=\"never\">");
         }
-
-        // Replace with absolute url
-        Elements scripts = doc.select("script[src]");
-        for (Element script : scripts) {
-            String src = script.absUrl("src");
-            if (!StringUtils.isEmpty(src)) {
-                script.attr("src", src);
-            }
-        }
-        for (Element link : doc.select("link[href]")) {
-            link.attr("href", link.absUrl("href"));
-        }
-        for (Element a : doc.select("a[href]")) {
-            a.attr("href", "/proxy/page/" + encodeBase64Url(a.absUrl("href")));
-        }
+        doc.head().append("<meta name=\"Content-Security-Policy\" content=\"default-src 'self'\">");
 
         // Add hook script
-        addHook(doc, url);
+        String protocol = url.getProtocol() + "://";
+        String host = url.getHost();
+        String port = url.getPort() == -1 ? "" : ":" + url.getPort();
+        String base = protocol + host + port;
+        doc.head().prepend(String.format(HOOK, base, url.getFile()));
 
         return new ResponseEntity<>(doc.toString().getBytes(doc.charset()), HttpStatus.OK);
     }
 
 
     // ------------ Util Methods ------------ //
-    private void addHook(Document doc, URL url) {
-        String protocol = url.getProtocol() + "://";
-        String host = url.getHost();
-        String port = url.getPort() == -1 ? "" : ":" + url.getPort();
-        String base = protocol + host + port;
-        doc.head().prepend(String.format(HOOK, base, url.getFile()));
-    }
-    private HttpHeaders getHeaders(HttpServletRequest request, String host) {
+    private HttpHeaders getHeaders(HttpServletRequest request, URL url) {
 
         HttpHeaders headers = new HttpHeaders();
         Enumeration<String> headerNames = request.getHeaderNames();
@@ -145,7 +128,11 @@ public class ProxyController {
             String headerValue = request.getHeader(headerName);
 
             if(headerName != null && headerValue != null) {
-                headers.add(headerName, headerName.toLowerCase().equals("host") ? host : headerValue);
+                switch (headerName.toLowerCase()) {
+                    case "host": headerValue = url.getHost(); break;
+                    case "referer": headerValue = url.toString(); break;
+                }
+                headers.add(headerName, headerValue);
             }
         }
 
