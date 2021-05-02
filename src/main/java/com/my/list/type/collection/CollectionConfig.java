3cbcebf3985905ebf0f88c34ff25e281a1111aa8
Kaixinguo <kaixinguo@kaixinguo.site>
2021-05-03 01:52:01 +0800

    Update ExcerptGenerator of Collection (New preview of collection)

--- src/main/java/com/my/list/type/collection/CollectionConfig.java
+++ src/main/java/com/my/list/type/collection/CollectionConfig.java
@@ -2,8 +2,6 @@ package com.my.list.type.collection;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.my.list.domain.MainData;
-import com.my.list.dto.ListItem;
 import com.my.list.dto.Node;
 import com.my.list.dto.Type;
 import com.my.list.dto.TypeConfig;
@@ -12,9 +10,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class CollectionConfig {
@@ -41,20 +41,30 @@ public class CollectionConfig {
     }
 
     private String generateExcerpt(Node node) {
-        List<ListItem> list = node.getExtraList();
+        List<Node> children = node.getExtraList().stream().map(i -> i.node).collect(Collectors.toList());
         Map<String, Object> excerpt = new HashMap<>();
-        if (list.size() == 0) {
+        List<Map<String, Object>> excerpts = new ArrayList<>();
+        if (children.size() == 0) {
             String text = node.getMainData().getTitle();
             if (StringUtils.isEmpty(text)) text = node.getMainData().getDescription();
             text = MyStringUtils.limit(text, 36);
-            excerpt.put("type", "node");
-            excerpt.put("excerpt", text);
+
+            Map<String, Object> e = new HashMap<>();
+            e.put("type", "node");
+            e.put("excerpt", text);
+            excerpts.add(e);
+
+            excerpt.put("excerpts", excerpts);
             excerpt.put("count", 0);
         } else {
-            MainData first = list.get(0).getNode().getMainData();
-            excerpt.put("type", first.getType());
-            excerpt.put("excerpt", first.getExcerpt());
-            excerpt.put("count", list.size());
+            excerpts = children.stream().limit(6).map(n -> {
+                Map<String, Object> e = new HashMap<>();
+                e.put("type", n.getMainData().getType());
+                e.put("excerpt", n.getMainData().getExcerpt());
+                return e;
+            }).collect(Collectors.toList());
+            excerpt.put("excerpts", excerpts);
+            excerpt.put("count", children.size());
         }
         try {
             return objectMapper.writeValueAsString(excerpt);
